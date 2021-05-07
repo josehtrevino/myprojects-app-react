84e159cd965529efc1fe78bfc428f94797e48a57
Jose Hernandez <josemht.94@gmail.com>
2021-05-07 16:20:55 -0500

    media queries added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 
 import { Link } from 'react-router-dom';
 import classes from './Header.module.css';
 import CompletedContext from '../store/completed-context';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   const completedCtx = useContext(CompletedContext);
 
   return (
     <header className={classes.header}>
       <div className={classes.logo}>My Projects</div>
       <nav>
-        <ul>
+        <ul className={`${classes.menu} ${isOpen ? classes.show : ''}`}>
           <li>
             <Link to="/">All Projects</Link>
           </li>
@@ -28,6 +29,13 @@ function Header() {
           </li>
         </ul>
       </nav>
+      <div onClick={() => setIsOpen(!isOpen)} className={classes.burger}>
+        <svg viewBox="0 0 100 80" width="40" height="40">
+          <rect width="100" height="20"></rect>
+          <rect y="30" width="100" height="20"></rect>
+          <rect y="60" width="100" height="20"></rect>
+        </svg>
+      </div>
     </header>
   );
 }
